cc4b1f0ab1334dd8a3d5af5b78130ab3fb47cc05
Taras Stekhnovych <Taras.Stekhnovych@symphony-solutions.eu>
2019-06-12 23:53:15 +0300

    Added snake body

--- src/classes/snake.class.js
+++ src/classes/snake.class.js
@@ -7,48 +7,94 @@ export class Snake {
     currentY = 10;
     currentDirection = 'right';
 
+    snakeBody = [];
+
     constructor(radius = 10, step = 5) {
         this.radius = radius;
         this.step = step;
+        this.snakeBody = [{
+            part: 'head',
+            x: this.currentX,
+            y: this.currentY
+        }, {x: 5, y: 10}, {x:0, y: 10}];
     }
 
     draw = (context) => {
         const startAngle = 0;
         const endAngle = 2 * Math.PI;
         context.beginPath();
-        context.arc(this.currentX, this.currentY, this.radius, startAngle, endAngle);
+        this.snakeBody.forEach((bodyPart) =>
+          context.arc(bodyPart.x, bodyPart.y, this.radius, startAngle, endAngle));
+        // context.arc(this.currentX, this.currentY, this.radius, startAngle, endAngle);
         context.fillStyle = '#FF32CD';
         context.fill();
     }
 
     move = (fieldWidth, fieldHeight) => {
         const minFieldValue = 0;
+        let { x, y } = this.snakeBody[0];
+
         switch (this.currentDirection) {
             case 'right':
-                this.currentX += this.step;
-                if (this.currentX >= fieldWidth) {
-                    this.currentX = this.radius;
+                //this.currentX += this.step;
+                x += this.step;
+                if (x >= fieldWidth) {
+                    // this.currentX = this.radius;
+                    x = this.radius;
                 }
+                this.moveSnakeHead({ x });
                 break;
             case 'left':
-                this.currentX -= this.step;
-                if (this.currentX <= minFieldValue) {
-                    this.currentX = fieldWidth - this.radius;
+                // this.currentX -= this.step;
+                x -= this.step;
+                if (x <= minFieldValue) {
+                    // this.currentX = fieldWidth - this.radius;
+                    x = fieldWidth - this.radius;
                 }
+                this.moveSnakeHead({ x });
                 break;
             case 'up':
-                this.currentY -= this.step;
-                if (this.currentY <= minFieldValue) {
-                    this.currentY = fieldHeight - this.radius;
+                // this.currentY -= this.step;
+                y -= this.step;
+                if (y <= minFieldValue) {
+                    // this.currentY = fieldHeight - this.radius;
+                    y = fieldHeight - this.radius;
                 }
+                this.moveSnakeHead({ y });
                 break;
             case 'down':
-                this.currentY += this.step;
-                if (this.currentY >= fieldHeight) {
-                    this.currentY = this.radius;
+                // this.currentY += this.step;
+                y += this.step;
+                if (y >= fieldHeight) {
+                    // this.currentY = this.radius;
+                    y = this.radius;
                 }
+                this.moveSnakeHead({ y });
                 break;
         }
+    }
+
+    moveSnakeHead = (newCoordinates) => {
+        this.adjustSnakeBody();
+        this.snakeBody[0] = {
+            ...this.snakeBody[0],
+            ...newCoordinates
+        };
+        // this.adjustSnakeBody();
+    }
+
+    adjustSnakeBody = () => {
+        this.snakeBody.forEach((snakePart, index, ar) => {
+            if (index === 0) {
+                return;
+            }
+            const { x, y } = ar[index - 1];
+            ar[index] = {
+                ...snakePart,
+                x,
+                y
+            };
+        });
     }
 
     changeDirection = (key) => {
